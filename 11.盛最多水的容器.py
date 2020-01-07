2e38654a9c0cb6f720cafb17e20583fb5b762f09
pjx206 <pjx206@163.com>
2020-01-08 00:36:02 +0800

    add unfinished solution and went to sleep

---
+++ 11.盛最多水的容器.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=11 lang=python3
+#
+# [11] 盛最多水的容器
+#
+
+# @lc code=start
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        if len(height) == 2:
+            return min(height)
+        
+        
+        
+# @lc code=end
+
