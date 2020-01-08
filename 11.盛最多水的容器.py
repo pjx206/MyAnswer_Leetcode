cf0e52a82f6da4add7b9d48e1f427c599dfc5815
pjx206 <pjx206@163.com>
2020-01-08 10:43:10 +0800

    TLE solution

--- 11.盛最多水的容器.py
+++ 11.盛最多水的容器.py
@@ -7,9 +7,27 @@
 # @lc code=start
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        minH = min(height)
         if len(height) == 2:
-            return min(height)
+            return minH
+        maxH = max(height)
+
+        maxArea = 0
+        # give a lid from bottom(minH) to the top
+        for lid in range(minH, maxH + 1):
+            left, right = 0, len(height) - 1
+            while height[left] < lid:
+                left += 1
+            while height[right] < lid:
+                right -= 1
+            if left == right:
+                break
+            lid = min(height[left], height[right])
+            maxArea = max(maxArea, (right-left)*lid)
         
+        return maxArea
+            
+
         
         
 # @lc code=end
